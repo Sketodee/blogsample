264f407fb070cef783cf93f951368d6d86b6f350
sketodee <shobandejames@gmail.com>
2022-09-05 10:27:35 +0100

    hot fixes from admin

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import postlist from "../posts.json"
 
 const Blog = () => {
+    console.log(postlist)
   return (
-    <h2>Blog</h2>
+    postlist.map((post, index) => {
+        return (
+            <div key={post.id}> 
+                <h2> {post.title} </h2>
+                <p> published by {post.author} on {post.date} </p>
+                <img src={post.thumbnail}/>
+            </div>
+        )
+    })
   )
 }
 
